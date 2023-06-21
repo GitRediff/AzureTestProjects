67b4a4b6da3c017d9c5156313b8727b589fbf431
Prasad <kooldreamz@outlook.com>
2023-06-21 19:44:59 +0530

    Added Brand controller for WebApi app Changed WebApp2 index file for CI github test

--- Src/WebApi/CrudOpsCoreVersion6/Controllers/BrandController.cs
+++ Src/WebApi/CrudOpsCoreVersion6/Controllers/BrandController.cs
@@ -9,6 +9,44 @@ namespace CrudOpsCoreVersion6.Controllers
     [ApiController]
     public class BrandController : ControllerBase
     {
-        
+        private readonly BrandContext _context;
+
+        public BrandController(BrandContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
+        {
+            if (_context.Brands == null)
+            { 
+                return NotFound(); 
+            }
+
+            return await _context.Brands.ToListAsync();
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<Brand>> GetBrandById(int id)
+        {
+            var brand = await _context.Brands.FindAsync(id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return brand;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Brand>> PostBrand (Brand brand)
+        {
+            _context.Brands.Add(brand);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBrandById), new {id = brand.ID}, brand);
+        }
     }
 }
