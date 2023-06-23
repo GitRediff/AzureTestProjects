48f7504f747dbe3a6297dbfa0762f0fcacc776da
Prasad <kooldreamz@outlook.com>
2023-06-23 15:22:44 +0530

    code completed... but getting error Creating new project

--- Src/AzureFunction/FunctionApp2/ImageResizeFunction.cs
+++ Src/AzureFunction/FunctionApp2/ImageResizeFunction.cs
@@ -3,6 +3,9 @@ using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
 
 namespace FunctionApp2
 {
@@ -10,9 +13,19 @@ namespace FunctionApp2
     public class ImageResizeFunction
     {
         [FunctionName("ImageResizeFunction")]
-        public void Run([BlobTrigger("prabhohighsize/{name}")]Stream myBlob, string name, ILogger log)
+        public void Run(
+            [BlobTrigger("prabhohighsize/{name}")] Stream inputBlob,
+            [Blob("prabholowsize/{name}", FileAccess.Write)] Stream outputBlob,
+            string name, 
+            ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            using (Image image = Image.Load(inputBlob))
+            {
+                image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+                image.Save(outputBlob, new JpegEncoder());
+            }
+
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {inputBlob.Length} Bytes");
         }
     }
 }
