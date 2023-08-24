ce89502052de239e71b4355ffee9378e8a6bccb4
Prasad <kooldreamz@outlook.com>
2023-08-24 18:05:40 +0530

    Sample Test

--- Src/Console/ConsoleSamples/Program.cs
+++ Src/Console/ConsoleSamples/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Warrior> warriors = new List<Warrior>()
+            {
+                new Warrior() { Name="Prasad", Height=172 },
+                new Warrior() { Name="Mahesh", Height=169 },
+                new Warrior() { Name="Mahendra", Height=172 }
+            };
+
+            var output = warriors
+                        .GroupBy(x => x.Height)//.ToList();
+                        .Select(x => new
+                        {
+                            value = x.Key,
+                            count = x.Count()
+                        }
+                        );
+
+            foreach (var item in output)
+            {
+                //Console.WriteLine(item.Key + " " + item.Count());
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
         }
+    }
+
+    internal class Warrior
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
     }
 }
